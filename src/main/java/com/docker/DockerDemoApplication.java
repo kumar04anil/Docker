5c5bfd93b8776940000c6e6b78532fcd521be096
aks <kumar.04anil@gmail.com>
2024-09-02 17:46:49 +0530

    docker basic project for test.

---
+++ src/main/java/com/docker/DockerDemoApplication.java
@@ -0,0 +1,22 @@
+package com.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String welcome(){
+		return "Welcome docker world!";
+	}
+
+}
